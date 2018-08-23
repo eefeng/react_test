6540332af8f1e62954470cb8627b0d328a2424bb
Feng <eefeng@vip.qq.com>
2018-08-24 02:27:55 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+console.log(React.createElement(
+  "h1",
+  null,
+  "hello"
+))
+
+
+ReactDOM.render(React.createElement(
+  "h1",
+  null,
+  "hello"
+), document.getElementById('root'));
 
